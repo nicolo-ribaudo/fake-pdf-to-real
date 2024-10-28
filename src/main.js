9a11182ddf7564ddf2c2680b2256e1d172fcfbc6
Nicolò Ribaudo <hello@nicr.dev>
2024-10-28 12:08:54 +0100

    Make it client-only

--- src/main.js
+++ src/main.js
@@ -1,6 +1,54 @@
-// ASSUMPTION: Most pages contain an image
-export function extractPages() {
-  const images = document.querySelectorAll("img[src^='data:']");
+import { PDFDocument, asPDFNumber, rgb } from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
+
+const pause = () => new Promise(resolve => setTimeout(resolve, 0));;
+
+export async function convertToPdf($container, onProgress = console.info) {
+  onProgress("Analyzing file...");
+  await pause();
+  const pages = extractPages($container);
+  if (!pages) return null;
+
+  onProgress(`Converting ${pages.length} pages...`);
+  await pause();
+
+  const pdf = await PDFDocument.create();
+  pdf.registerFontkit(fontkit);
+
+  const pdfFontsPromises = new Map();
+  const getFont = async (fontFamily) => {
+    let fontP = pdfFontsPromises.get(fontFamily);
+    if (!fontP) {
+      fontP = pdf.embedFont(extractFontURL($container, fontFamily));
+      pdfFontsPromises.set(fontFamily, fontP);
+    }
+    return fontP;
+  }
+
+  let remaining = pages.length;
+  await Promise.all(
+    pages.map(async (page, i) => {
+      await pause();
+      const pdfPage = pdf.addPage([page.width, page.height]);
+      await processPage(pdf, pdfPage, page, getFont);
+      onProgress(`Page ${String(i + 1).padStart(3, " ")} converted (${--remaining} remaining).`);
+      await pause();
+    })
+  );
+
+  await pause();
+  onProgress("Generating PDF file...");
+  await pause();
+
+  const result = await pdf.save();
+
+  onProgress("Done");
+
+  return result;
+}
+
+function extractPages($container) {
+  const images = $container.querySelectorAll("img[src^='data:']");
 
   const nodes = new Map();
 
@@ -18,7 +66,8 @@ export function extractPages() {
     return node;
   };
 
-  let root = createNode(TYPE_CONTAINER, document.body, []);
+  let root = createNode(TYPE_CONTAINER, $container, []);
+  nodes.set($container, root);
 
   for (const image of images) {
     let node = createNode(TYPE_IMAGE, image, []);
@@ -192,16 +241,77 @@ export function extractPages() {
   return pages;
 }
 
-export function extractFont(fontFamily) {
-  for (const styleSheet of document.styleSheets) {
-    for (const rule of styleSheet.rules) {
-      if (
-        rule instanceof CSSFontFaceRule &&
-        rule.style.getPropertyValue("font-family") === fontFamily
-      ) {
-        return rule.style.getPropertyValue("src");
+async function processPage(pdf, pdfPage, page, getFont) {
+  for (const image of page.images) {
+    const pdfImg = await pdf.embedPng(image.src);
+    pdfPage.drawImage(pdfImg, {
+      width: image.width,
+      height: image.height,
+      x: image.left,
+      y: image.bottom,
+    });
+  }
+
+  for (const text of page.text) {
+    const font = await getFont(text.font);
+
+    let opacity = text.opacity;
+    let color =
+      text.color == null ? undefined : rgb(...text.color.map((c) => c / 255));
+
+    pdfPage.drawText(text.text, {
+      font,
+      x: text.left,
+      y: text.bottom,
+      size: text.size,
+      opacity: opacity,
+      color,
+    });
+
+    if (text.transform) {
+      const { operators } = pdfPage.contentStream;
+      for (let i = operators.length - 1; i >= 0; i--) {
+        if (operators[i].name === "q") {
+          break; // PushGraphcsState
+        }
+        if (operators[i].name === "Tm") {
+          const matrix = new DOMMatrix(
+            operators[i].args.map((n) => n.numberValue)
+          ).multiply(new DOMMatrix(text.transform));
+          operators[i].args = [
+            matrix.a,
+            matrix.b,
+            matrix.c,
+            matrix.d,
+            matrix.e,
+            matrix.f,
+          ].map(asPDFNumber);
+          break;
+        }
       }
     }
   }
-  return null;
+}
+
+function extractFontURL($container, fontFamily) {
+  for (const styleSheet of $container.ownerDocument.styleSheets) {
+    for (const rule of styleSheet.rules) {
+      if (
+        rule.constructor.name ==="CSSFontFaceRule" &&
+        rule.style.getPropertyValue("font-family") === fontFamily
+      ) {
+        let src =  rule.style.getPropertyValue("src");
+        if (src.startsWith("url(")) {
+          src = src.slice("url(".length, -")".length);
+          if (src.startsWith('"') || src.startsWith("'")) src = src.slice(1, -1);
+
+          return src;
+        } else {
+          throw new Error(`Cannot extract font ${fontFamily} with src ${src}`);
+        }
+      }
+    }
+  }
+
+  throw new Error(`Cannot find font ${fontFamily}`);
 }
